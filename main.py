8bff6d50b9bb2480ccb669ddfc4de918edb759a0
Dave PH Lin <pohanlin2mit.edu>
2018-12-11 23:48:17 -0500

    show all out contours

--- main.py
+++ main.py
@@ -2,31 +2,13 @@ import numpy as np
 import cv2
 from collections import defaultdict
 
-img = cv2.imread('example_01.jpg')
-img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(img_gray,127,255,0)
-img2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-contours = sorted(contours, key=lambda contours: get_roi_area(contours)[1])
-contours = sorted_contours[1:]
 
-levels = defaultdict(list)
-for i in range(len(contours)-1):
-	for j in range(i, len(contours)):
-		contour1, contour2 = contours[i], contours[j]
-		if inside(contour1,contour2):
-			levels[i].append(j)
-		elif inside(contour2,contour1):
-			levels[j].append(i)
-
-out_contours = list(levels.keys())
-
-
-
-def get_roi_area(contour):
+def get_roi_area_coords(contour):
 	x, y, w, h = cv2.boundingRect(contour)
 	roi = img[y:y + h, x:x + w]
 	area = w*h
-	return roi, area
+	coords = [x, y, x+w, y+h]
+	return roi, area, coords
 
 def inside(contour1,contour2):
 	xs1, ys1, w1, h1 = cv2.boundingRect(contour1)
@@ -37,16 +19,42 @@ def inside(contour1,contour2):
 
 
 
-for i, contour in enumerate(sorted_contours):
-    x, y, w, h = cv2.boundingRect(contour)
+img = cv2.imread('example_01.jpg')
+img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(img_gray,127,255,0)
+img2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+contours = sorted(contours, key=lambda contour: get_roi_area_coords(contour)[1], reverse=True)
+# cv2.drawContours(img, contours, 0, (0,255,0), 3)
+# cv2.imshow('img', img)
+# cv2.waitKey(5000)
+contours = contours[1:]
 
-    roi = img[y:y + h, x:x + w]
-    area = w*h
+levels = defaultdict(list)
+for i in range(len(contours)-1):
+	for j in range(i, len(contours)):
+		contour1, contour2 = contours[i], contours[j]
+		if inside(contour1,contour2):
+			levels[i].append(j)
+		elif inside(contour2,contour1):
+			levels[j].append(i)
 
-    if 250 < area:
-        rect = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.imshow('rect', rect)
+in_contours = list(levels.values())
+in_contours = [_[0] for _ in in_contours]
+out_contours = [_ for _ in range(len(contours)) if _ not in in_contours]
 
-cv2.waitKey(10000)
+for i in out_contours:
+	contour = contours[i]
+	roi, area, coords = get_roi_area_coords(contour)
+	xs, ys, xn, yn = coords
+	rect = cv2.rectangle(img, (xs, ys), (xn, yn), (0, 255, 0), 2)
+	cv2.imshow('rect', rect)
+
+cv2.waitKey(5000)
+
+
+
+
+
+
 
 
